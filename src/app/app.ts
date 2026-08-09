1b27069cac29f8b2ee336b9966e911a6fa1cf771
Jansch2205 <felix-jaensch@freenet.de>
2026-08-09 22:03:20 +0200

    Commands removed

--- src/app/app.ts
+++ src/app/app.ts
@@ -16,12 +16,10 @@ export class AppComponent implements OnInit { // 2. AppComponent statt App
   ngOnInit() {
     App.addListener('backButton', ({ canGoBack }) => {
       if (canGoBack) {
-        // Geht eine Seite in der Angular-Historie zurück
         this.location.back();
       } else {
-        // Schließt die App nur, wenn keine vorherige Seite mehr existiert
         App.exitApp();
       }
     });
-  } // 3. Klammer für ngOnInit() ergänzt
+  }
 }
