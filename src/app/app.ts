090b80117eff8a9e1997d685936647ff48724f15
Jansch2205 <felix-jaensch@freenet.de>
2026-08-09 21:50:52 +0200

    Back gesture fixed

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Location } from '@angular/common'; // 1. Location importieren
+import { App } from '@capacitor/app';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,19 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {}
+export class AppComponent implements OnInit { // 2. AppComponent statt App
+
+  constructor(private location: Location) {}
+
+  ngOnInit() {
+    App.addListener('backButton', ({ canGoBack }) => {
+      if (canGoBack) {
+        // Geht eine Seite in der Angular-Historie zurück
+        this.location.back();
+      } else {
+        // Schließt die App nur, wenn keine vorherige Seite mehr existiert
+        App.exitApp();
+      }
+    });
+  } // 3. Klammer für ngOnInit() ergänzt
+}
